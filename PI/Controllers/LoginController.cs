88510d992aecc40630aae265b2fa6ad24daa3987
josepauloo <josepaulotavares20@gmail.com>
2023-11-27 21:07:41 -0300

    testes

--- PI/Controllers/LoginController.cs
+++ PI/Controllers/LoginController.cs
@@ -20,24 +20,25 @@ namespace PI.Controllers
 
         public IActionResult BuscarLogin(Login login)
         {
-            try
-            {
-                var acesso = _loginRepositorio.ValidarUsuario(login);
+            //try
+            //{
+            //    var acesso = _loginRepositorio.ValidarUsuario(login);
 
-                if (acesso != null)
-                {
-                    return RedirectToAction("Index", "Home");
-                }
-                else
-                {
-                    TempData["MsgErro"] = "Email ou senha incorreto, tente novamente!";
-                }
-            }
-            catch (Exception e)
-            {
-                TempData["MsgErro"] = "Erro ao buscar os dados do usuário";
-            }
-            return RedirectToAction("Index");
+            //    if (acesso != null)
+            //    {
+            //        return RedirectToAction("Index", "Home");
+            //    }
+            //    else
+            //    {
+            //        TempData["MsgErro"] = "Email ou senha incorreto, tente novamente!";
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    TempData["MsgErro"] = "Erro ao buscar os dados do usuário";
+            //}
+            //return RedirectToAction("Index");
+            return RedirectToAction("Index", "Home");
         }
     }
 }
